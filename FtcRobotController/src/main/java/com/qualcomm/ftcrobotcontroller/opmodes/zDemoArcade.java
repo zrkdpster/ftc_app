23ae18b840655fc69fec144638c2b5a952ae33ae
jjkd <jjkd@jjkd.com>
2015-11-17 21:36:44 -0600

    Cleaned up opmodes for hardware update

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/zDemoArcade.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/zDemoArcade.java
@@ -11,25 +11,16 @@ public class zDemoArcade extends OpMode {
     final double DOWN_POSITION = 1.0;
 
     DcMotor leftFrontMotor;
-    DcMotor leftRearMotor;
     DcMotor rightFrontMotor;
-    DcMotor rightRearMotor;
-    DcMotor spinnerMotor;
-    Servo gripper;
 
     @Override
     public void init() {
 
         leftFrontMotor = hardwareMap.dcMotor.get("left_front");
-        leftRearMotor = hardwareMap.dcMotor.get("left_rear");
         rightFrontMotor = hardwareMap.dcMotor.get("right_front");
-        rightRearMotor = hardwareMap.dcMotor.get("right_rear");
-        spinnerMotor = hardwareMap.dcMotor.get("spinner");
 
         rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
-        rightRearMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        gripper.setPosition(UP_POSITION);
     }
 
     @Override
@@ -63,28 +54,9 @@ public class zDemoArcade extends OpMode {
         rightPower = Range.clip(rightPower, -1, 1);
 
         leftFrontMotor.setPower(leftPower);
-        leftRearMotor.setPower(leftPower);
 
         rightFrontMotor.setPower(rightPower);
-        rightRearMotor.setPower(rightPower);
 
-        // operate spinner motors, use gamepad buttons y and b
 
-        if(gamepad1.y) {
-            spinnerMotor.setPower(0.1);
-        } else if(gamepad1.b) {
-            spinnerMotor.setPower(-0.1);
-        } else {
-            spinnerMotor.setPower(0);
-        }
-
-        // control the gripper
-
-        if(gamepad1.x) {
-            gripper.setPosition(UP_POSITION);
-        }
-        if(gamepad1.a) {
-            gripper.setPosition(DOWN_POSITION);
-        }
     }
 }
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/zDemoTank.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/zDemoTank.java
@@ -6,29 +6,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class zDemoTank extends OpMode {
 
-    final double UP_POSITION = 0.0;
-    final double DOWN_POSITION = 1.0;
-
     DcMotor leftFrontMotor;
-    DcMotor leftRearMotor;
     DcMotor rightFrontMotor;
-    DcMotor rightRearMotor;
-    DcMotor spinnerMotor;
-    Servo gripper;
 
     @Override
     public void init() {
 
         leftFrontMotor = hardwareMap.dcMotor.get("left_front");
-        leftRearMotor = hardwareMap.dcMotor.get("left_rear");
         rightFrontMotor = hardwareMap.dcMotor.get("right_front");
-        rightRearMotor = hardwareMap.dcMotor.get("right_rear");
-        spinnerMotor = hardwareMap.dcMotor.get("spinner");
 
         rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
-        rightRearMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        gripper.setPosition(UP_POSITION);
     }
 
     @Override
@@ -56,28 +44,8 @@ public class zDemoTank extends OpMode {
         }
 
         leftFrontMotor.setPower(leftPower);
-        leftRearMotor.setPower(leftPower);
 
         rightFrontMotor.setPower(rightPower);
-        rightRearMotor.setPower(rightPower);
-
-        // operate spinner motors, use gamepad buttons y and b
-
-        if(gamepad1.y) {
-            spinnerMotor.setPower(0.1);
-        } else if(gamepad1.b) {
-            spinnerMotor.setPower(-0.1);
-        } else {
-            spinnerMotor.setPower(0);
-        }
-
-        // control the gripper
-
-        if(gamepad1.x) {
-            gripper.setPosition(UP_POSITION);
-        }
-        if(gamepad1.a) {
-            gripper.setPosition(DOWN_POSITION);
-        }
+       
     }
 }
