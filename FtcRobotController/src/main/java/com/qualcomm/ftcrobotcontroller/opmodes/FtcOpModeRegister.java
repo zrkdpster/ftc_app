5060c3e9a1236670d91546db09b9c01c05d55323
jjkd <jjkd@jjkd.com>
2015-11-17 21:14:36 -0600

    Add local demo opmodes

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -58,6 +58,9 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("NullOp", NullOp.class);
 
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
+    manager.register("NullOp", NullOp.class);
+    manager.register ("jjkdDemoTank", jjkdDemoTank.class);
+    manager.register ("jjkdDemoArcade", jjkdDemoArcade.class);
     manager.register("K9TeleOp", K9TeleOp.class);
     manager.register("K9Line", K9Line.class);
     manager.register ("PushBotAuto", PushBotAuto.class);
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/jjkdDemoTank.java
@@ -0,0 +1,83 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class jjkdDemoTank extends OpMode {
+
+    final double UP_POSITION = 0.0;
+    final double DOWN_POSITION = 1.0;
+
+    DcMotor leftFrontMotor;
+    DcMotor leftRearMotor;
+    DcMotor rightFrontMotor;
+    DcMotor rightRearMotor;
+    DcMotor spinnerMotor;
+    Servo gripper;
+
+    @Override
+    public void init() {
+
+        leftFrontMotor = hardwareMap.dcMotor.get("left_front");
+        leftRearMotor = hardwareMap.dcMotor.get("left_rear");
+        rightFrontMotor = hardwareMap.dcMotor.get("right_front");
+        rightRearMotor = hardwareMap.dcMotor.get("right_rear");
+        spinnerMotor = hardwareMap.dcMotor.get("spinner");
+
+        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightRearMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        gripper.setPosition(UP_POSITION);
+    }
+
+    @Override
+    public void loop() {
+
+        double leftY = -gamepad1.left_stick_y;
+        double rightY = -gamepad1.right_stick_y;
+
+        double leftSquaredVal = leftY * leftY;
+        double rightSquaredVal = rightY * rightY;
+
+        double leftPower;
+        double rightPower;
+
+        if(leftY < 0) {
+            leftPower = -leftSquaredVal;
+        } else {
+            leftPower = leftSquaredVal;
+        }
+
+        if(rightY < 0) {
+            rightPower = -rightSquaredVal;
+        } else {
+            rightPower = rightSquaredVal;
+        }
+
+        leftFrontMotor.setPower(leftPower);
+        leftRearMotor.setPower(leftPower);
+
+        rightFrontMotor.setPower(rightPower);
+        rightRearMotor.setPower(rightPower);
+
+        // operate spinner motors, use gamepad buttons y and b
+
+        if(gamepad1.y) {
+            spinnerMotor.setPower(0.1);
+        } else if(gamepad1.b) {
+            spinnerMotor.setPower(-0.1);
+        } else {
+            spinnerMotor.setPower(0);
+        }
+
+        // control the gripper
+
+        if(gamepad1.x) {
+            gripper.setPosition(UP_POSITION);
+        }
+        if(gamepad1.a) {
+            gripper.setPosition(DOWN_POSITION);
+        }
+    }
+}
