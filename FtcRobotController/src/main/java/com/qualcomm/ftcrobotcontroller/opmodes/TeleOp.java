e9998cd4a1bc4d2b9eea3fe5b58b7a96b95582c1
ftc5294 <ftc5294@gmail.com>
2015-12-11 21:43:29 -0600

    Added scale factors for arcade and tank mode in TeleOp OpNMode - allows reduced max power for full scale joystick.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
@@ -12,16 +12,27 @@ import com.qualcomm.robotcore.util.Range;
 public class TeleOp extends OpMode {
     DcMotor leftMotor;
     DcMotor rightMotor;
+    DcMotor elbowMotor;
+    DcMotor shoulderMotor;
 
-    Servo jjDumper;
-    Servo redNoodle;
-    Servo blueNoodle;
+
+    Servo dumperServo;
+    Servo redNoodleServo;
+    Servo blueNoodleServo;
 
     boolean arcademode;
 
     AdafruitIMU boschBNO055;
 
+    public int elbowMotorEncodercurrent;
+    public int shoulderMotorEncodercurrent;
 
+    public double tankMaxPower      = 1.0;
+    public double arcadeMaxPower    = 1.0;
+
+    public double maxXJoystick      = 1.0;
+
+    public int incremment = 5;
 
     @Override
     public void init() {
@@ -31,17 +42,24 @@ public class TeleOp extends OpMode {
         init_sensors();
 
     }
-
+//initialize drivetrain
     public void init_motors() {
         leftMotor = hardwareMap.dcMotor.get("left");
         rightMotor = hardwareMap.dcMotor.get("right");
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
         arcademode = true;
         telemetry.addData("Drive: ", "Arcade Mode");
+//initialize arm
+        elbowMotor = hardwareMap.dcMotor.get("elbow");
+        shoulderMotor = hardwareMap.dcMotor.get("shoulder");
+        //xMotor.setDirection(DcMotor.Direction.REVERSE); ******might need
+
     }
 
     public void init_servos() {
-
+        dumperServo = hardwareMap.servo.get("dumper");
+        redNoodleServo = hardwareMap.servo.get("redNoodle");
+        blueNoodleServo = hardwareMap.servo.get("blueNoodle");
     }
 
     public void init_sensors() {
@@ -53,8 +71,12 @@ public class TeleOp extends OpMode {
 
         if (arcademode) {    //  handle arcade mode here
 
+            telemetry.addData("Drive: ", "Arcade Mode");
+
             double xValue = -gamepad1.left_stick_x;
             double yValue = -gamepad1.left_stick_y;
+
+            xValue = xValue * maxXJoystick;
 
             double xSquaredVal = xValue * xValue;
             double ySquaredVal = yValue * yValue;
@@ -80,11 +102,14 @@ public class TeleOp extends OpMode {
             leftPower = Range.clip(leftPower, -1, 1);
             rightPower = Range.clip(rightPower, -1, 1);
 
-            leftMotor.setPower(leftPower);
+            leftMotor.setPower(leftPower * arcadeMaxPower);
 
-            rightMotor.setPower(rightPower);
+            rightMotor.setPower(rightPower * arcadeMaxPower);
 
         } else {            //  handle tank mode here
+
+            telemetry.addData("Drive: ", "Tank Mode");
+
             double leftY = -gamepad1.left_stick_y;
             double rightY = -gamepad1.right_stick_y;
 
@@ -106,9 +131,9 @@ public class TeleOp extends OpMode {
                 rightPower = rightSquaredVal;
             }
 
-            leftMotor.setPower(leftPower);
+            leftMotor.setPower(leftPower * tankMaxPower);
 
-            rightMotor.setPower(rightPower);
+            rightMotor.setPower(rightPower * tankMaxPower);
 
         }
 
@@ -128,12 +153,58 @@ public class TeleOp extends OpMode {
 
         }
     }
+    public void handle_arm() {
+
+        if (gamepad1.dpad_up){
+            telemetry.addData("Controller: ", "dpad_up");
+            elbowMotor.setTargetPosition(elbowMotor.getCurrentPosition() + incremment);
+        }
+        if (gamepad1.dpad_down){
+            telemetry.addData("Controller: ", "dpad_down");
+            elbowMotor.setTargetPosition(elbowMotor.getCurrentPosition() - incremment);
+        }
+        if (gamepad1.dpad_left){
+            telemetry.addData("Controller: ", "dpad_left");
+            shoulderMotor.setTargetPosition(shoulderMotor.getCurrentPosition() + incremment);
+        }
+        if (gamepad1.dpad_right){
+            telemetry.addData("Controller: ", "dpad_uright");
+            shoulderMotor.setTargetPosition(shoulderMotor.getCurrentPosition() - incremment);
+        }
+    }
+ public void handle_servos() {
+     if (gamepad1.a){
+         telemetry.addData("Controller: ", "a");
+         dumperServo.setPosition(1.0);
+     } else {
+         dumperServo.setPosition(0.0);
+     }
+
+     if (gamepad1.x){
+         telemetry.addData("Controller: ", "x");
+         redNoodleServo.setPosition(1.0);
+     } else {
+         redNoodleServo.setPosition(0.0);
+     }
+
+     if (gamepad1.y){
+         telemetry.addData("Controller: ", "y");
+         blueNoodleServo.setPosition(1.0);
+     } else {
+         blueNoodleServo.setPosition(0.0);
+     }
+
+
+ }
+
 
     @Override
     public void loop() {
 
         handle_drivetrain();
         handle_mode_commands();
+        handle_servos();
+        handle_arm();
 
     }
 }
